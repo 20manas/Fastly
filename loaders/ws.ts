45863e7de6ad748af4eedd62e06d8d8074b62ca2
Manas Khurana <manas.khurana20@gmail.com>
2020-11-19 17:00:02 +0530

    Improve type safety of messages.

--- loaders/ws.ts
+++ loaders/ws.ts
@@ -1,19 +1,50 @@
+import {Application} from 'express';
+import WebSocket from 'ws';
+import expressWs from 'express-ws';
+
 import {sendMessage} from '../services/messages';
+import {UserEssentials} from '../database/types/users';
 
-export default (app: any) => {
-  require('express-ws')(app);
+interface Chat {
+  sender: string;
+  receiver: string;
+  message: string;
+}
 
-  const users = new Map<string, any>();
+const isString = (obj: any): obj is string => typeof obj === 'string';
 
-  app.ws('/chat', (ws: any, req: any) => {
+const instanceOfChat = (obj: any): obj is Chat => {
+  return isString(obj.sender) && isString(obj.receiver) && isString(obj.message);
+};
+
+const isJSON = (obj: string) => {
+  try {
+    JSON.parse(obj);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export default (application: Application) => {
+  expressWs(application);
+  const app = application as expressWs.Application;
+
+  const users = new Map<string, WebSocket>();
+
+  app.ws('/chat', (ws, req) => {
     if (req.user) {
-      users.set(req.user.username, ws);
+      const user = req.user as UserEssentials;
+      users.set(user.username, ws);
     }
 
-    ws.on('message', async (message: any) => {
+    ws.on('message', async (message) => {
+      if (typeof message != 'string') return;
+
+      if (!isJSON(message)) return;
       const data = JSON.parse(message);
 
-      if (!data.sender || !data.receiver || !data.message) return;
+      if (!instanceOfChat(data)) return;
 
       if (users.get(data.sender) != ws) return;
 
@@ -21,9 +52,7 @@ export default (app: any) => {
 
       if (res === null) return;
 
-      if (users.has(data.receiver)) {
-        users.get(data.receiver).send(JSON.stringify(res));
-      }
+      users.get(data.receiver)?.send(JSON.stringify(res));
     });
   });
 };
--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,27 +1,30 @@
 import React, {useEffect, useState, FormEvent} from 'react';
+
 import {Layout} from '../components/layout';
 import loginStyles from '../styles/login.module.css';
 
-let ws: any = null;
+let ws: null | WebSocket = null;
 
 const sendMessage = (ev: FormEvent<HTMLFormElement>) => {
   ev.preventDefault();
 
-  if (!ws.send) return;
+  if (ws === null) return;
   ws.send(JSON.stringify({
-    sender: document.getElementsByName('sender')[0].value,
-    receiver: document.getElementsByName('receiver')[0].value,
-    message: document.getElementsByName('content')[0].value,
+    sender: (document.getElementsByName('sender')[0] as HTMLInputElement).value,
+    receiver: (document.getElementsByName('receiver')[0] as HTMLInputElement).value,
+    message: (document.getElementsByName('content')[0] as HTMLInputElement).value,
   }));
 };
 
 const Messages = () => {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(<></>);
 
   useEffect(() => {
-    ws = new WebSocket('ws://localhost:3000/message');
+    ws = new WebSocket('ws://localhost:3000/chat');
 
-    ws.addEventListener('open', () => ws.send(JSON.stringify({content: 'something from client'})));
+    ws.addEventListener('open', () => {
+      (ws as WebSocket).send(JSON.stringify({content: 'something from client'}));
+    });
 
     ws.addEventListener('message', (data: any) => {
       setMessages(<li>{data.data}</li>);
